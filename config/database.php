93e7a967802bc26458da6776a00746d477c84a17
Boluwatife2004 <119611414+Boluwatife2004@users.noreply.github.com>
2024-04-21 16:44:36 -0600

    Configure DB & Redis Variables

--- config/database.php
+++ config/database.php
@@ -45,10 +45,9 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
             'host' => env('AZURE_MYSQL_HOST', '127.0.0.1'),
             'port' => env('AZURE_MYSQL_PORT', '3306'),
-            'database' => env('AZURE_MYSQL_DBNAME', 'forge'),
+            'database' => env('AZURE_MYSQL_DBNAME', 'SQLWEB-APP'), // Adjusted database name
             'username' => env('AZURE_MYSQL_USERNAME', 'forge'),
             'password' => env('AZURE_MYSQL_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
@@ -62,6 +61,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        
 
         'pgsql' => [
             'driver' => 'pgsql',
